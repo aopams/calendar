8aa7c7c8c4eec0888f18e178a5bcdfe0b8eff962
William Truong <william_truong@brown.edu>
2015-04-02 14:33:26 -0400

    added run spark server stuff

--- src/main/java/edu/brown/cs/andrew/calendar/Main.java
+++ src/main/java/edu/brown/cs/andrew/calendar/Main.java
@@ -1,5 +1,9 @@
 package edu.brown.cs.andrew.calendar;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringWriter;
 import java.sql.SQLException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -8,10 +12,24 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+import spark.ExceptionHandler;
+import spark.ModelAndView;
+import spark.QueryParamsMap;
+import spark.Request;
+import spark.Response;
+import spark.Route;
+import spark.Spark;
+import spark.TemplateViewRoute;
+import spark.template.freemarker.FreeMarkerEngine;
 import edu.brown.cs.andrew.handlers.DatabaseHandler;
 import edu.brown.cs.andrew.handlers.Event;
+import freemarker.template.Configuration;
 
 public class Main {
+  
+  private static final int RESSTAT = 500;
   
   public static void main(String[] args) {
     System.out.println("Hello World");
@@ -55,6 +73,58 @@ public class Main {
       System.out.println(System.currentTimeMillis()/1000);
     } catch (ClassNotFoundException | SQLException | ParseException e) {
       e.printStackTrace();
+      //run spark server after andrew's testing
+    } finally {
+      run(args);
+    }
+  }
+  
+  /**
+   * run() method handles command line input to run program
+   * with gui if "--gui" is indicated
+   */
+  private static void run(String[] args) {
+    OptionParser parser = new OptionParser();
+    parser.accepts("gui");
+    OptionSet options = parser.parse(args);
+    if (options.has("gui")) {
+      runSparkServer();
+    }
+  }
+
+  private static FreeMarkerEngine createEngine() {
+    Configuration config = new Configuration();
+    File templates = new File(
+      "src/main/resources/spark/template/freemarker");
+    try {
+      config.setDirectoryForTemplateLoading(templates);
+    } catch (IOException ioe) {
+      System.out.printf("ERROR: Unable "
+          + "use %s for template loading.\n", templates);
+      System.exit(1);
+    }
+    return new FreeMarkerEngine(config);
+  }
+  
+  private static void runSparkServer() {
+	    Spark.externalStaticFileLocation("src/main/resources/static");
+	    Spark.setPort(1234);
+	    Spark.exception(Exception.class, new ExceptionPrinter());
+	    FreeMarkerEngine freeMarker = createEngine();
+	    // Setup Spark Routes
+  }
+  
+  private static class ExceptionPrinter implements ExceptionHandler {
+    @Override
+    public void handle(Exception e, Request req, Response res) {
+      res.status(RESSTAT);
+      StringWriter stacktrace = new StringWriter();
+      try (PrintWriter pw = new PrintWriter(stacktrace)) {
+        pw.println("<pre>");
+        e.printStackTrace(pw);
+        pw.println("</pre>");
+      }
+      res.body(stacktrace.toString());
     }
   }
 }
