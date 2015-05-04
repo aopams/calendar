58b248203bd8565b594d442be7961002c1f9dcee
hyeddana <hnyeddanapudy@hotmail.com>
2015-05-04 01:41:49 -0400

    fixed new ev dialog

--- src/main/resources/static/js/calendar.js
+++ src/main/resources/static/js/calendar.js
@@ -121,7 +121,6 @@ function newEvent() {
 
 /* user edits her own event. */
 function editEvent(id) {
-console.log('id is ' + id);
 	var title = document.getElementById('title').value;
 	var date = document.getElementById('datepicker').value;
 	var time = document.getElementById('dialog-time').value;
@@ -142,7 +141,7 @@ console.log('id is ' + id);
 		console.log(responseObject);
 	    if(responseObject.status != 1) {
 	    	console.log('going to ranking functions');
-			displayRanking(responseObject, 1);
+			displayRanking(responseObject, id);
 		} else {
 			var $dialog = $('.ui-dialog-content');
 		    $dialog.dialog('destroy');
@@ -251,57 +250,37 @@ function openDialog(key, google) {
 }
 
 /* opens dialog window for events that creators have control over editing */
-function openDialog(key, google) {
-	value = eventMap[key.id];
-	var date = value.date.split(" ");
-	var day = date[0] + " " + date[1] + " " + date[2];
-	var T = date[3];
-	var am = date[4];
-	var time = date[3].substring(0, T.length - 3) + " " + am;
-	var dur = value.duration;
-	if (google == 1 || google == 2) {
-		var ds = 'disabled';
-	} else {
-		var ds = '';
-	}
+function newEventDialog(date, time) {
 	form =
 	'<form class="form-inline" id ="newEventForm">' +
-	'<div id="dialog-event-id">' + value.id + '</div>' +
 	'<div class="form-group dialog-form">' +
 		'<img id="x-button" src="/img/x.png"/>' +
 	    '<div class="input-group">' +
-		    '<input type="text" class="form-control" id="title" placeholder="Title" value="' + value.title + '"'+ ds +'>' +
+		    '<input type="text" class="form-control" id="title" placeholder="Title">' +
 	    '</div>' +
 	    '<div class="input-group margin-group">' +
 		    '<div class="input-group-addon"><span class="glyphicon glyphicon-calendar" aria-hidden="true"></span></div>' + 
-		    '<input type="text" class="form-control" id="datepicker" placeholder="date" value="' + day + '"'+ ds +'>' +
+		    '<input type="text" class="form-control" id="datepicker" placeholder="date" value="' + date + '">' +
 	    '</div>' +
 	    '<div class="input-group margin-group">' +
 	    	'<div class="formatted">At </div>' +
-	    		'<input id="dialog-time" type="text" class="form-control" onclick="" value="' + time + '"'+ ds +' >' +
-			'<div class="formatted">   for </div>' +
-				'<input id="duration" type="text" class="form-control" value="' + dur + '"'+ ds +'><div class="formatted"> min </div>' +
+	    		'<input id="dialog-time" type="text" class="form-control" value="' + time + '">' +
+			'<div class="formatted">  for </div>' +
+				'<input id="duration" type="text" class="form-control" value="60"><div class="formatted"> min </div>' +
 	    '</div>' +
 	    '<div class="input-group margin-group">' +
-	    	'<textarea type="text" class="form-control" id="descrip" placeholder="description..."'+ ds +'>'+ value.description + '</textarea>' +
+	    	'<textarea type="text" class="form-control" id="descrip" placeholder="description..."></textarea>' +
 	    '</div>' +
 	    '<div class="input-group margin-group">' +
-	    	'<div class="input-group-addon">@</div><input type="text" class="form-control" id="attendees" placeholder="People" value="' 	 				+ value.attendees +'"'+ ds +'/>' +
+	    	'<div class="input-group-addon">@</div><input type="text" class="form-control" id="attendees" placeholder="People" value=""/>'+
 		'</div>' +
 	  	'<div class="input-group margin-group">' +
 		    '<div class="input-group-addon">@</div>' +
-		    '<input type="text" class="form-control" id="group" placeholder="Group" value="'+ value.group +'"'+ ds +'/>' +
+		    '<input type="text" class="form-control" id="group" placeholder="Group"/>' +
 		'</div>' +
-		'<div class="margin-group-xl">';
-
-	if (google == 1) {
-		form = form + '<img id="google-button" src="\\img/google.png"/>';
-	} else if (google == 2) {
-		form = form + '<img id="remove-button" src="\\img/minus.png"/>';
-	} else {
-		form = form + '<img id="delete-button" src="\\img/minus.png"/><img id="edit-button" src="\\img/check.png"/>';
-	}
-	form = form + '</div> </div> </form>';
+		'<div class="margin-group-xl">'+ 
+		'<img id="new-event-button" src="\\img/check.png"/>' +
+		'</div> </div> </form>';
 	$(form).dialog({ modal: true, resizable: false});
 }
 
@@ -650,6 +629,7 @@ function timeOffsetMargin() {
 			currMarg = 0;
 		}
 		 else {
+		 console.log(currMarg);
 			currMarg = currMarg.substring(0, currMarg.length - 2);
 		}
 		var newMarg = (currMarg * 1) + getTimeOffsetMargin(evTime);
